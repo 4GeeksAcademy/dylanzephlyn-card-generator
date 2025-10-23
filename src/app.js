4f7212c73ac4c3e3d2a9783b3dc5b37053954893
Dylan Steven Inchaurregui Dooley <dylan.zephlyn@hotmail.com>
2025-10-23 19:54:08 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,57 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+    let cardGenerated = "";
+
+    let symbols = ['♠', '♥', '♦', '♣'];
+    let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', 'Q'];
+
+    function generateRandomCard() {
+        let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+        let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+        
+        let colorClass;
+        if (randomSymbol === '♥' || randomSymbol === '♦') {
+            colorClass = 'text-danger';
+        } else {
+            colorClass = 'text-dark';
+        }
+        
+        document.querySelector("#cardTop").innerHTML = `<span class="${colorClass}">${randomSymbol}</span>`;
+        document.querySelector("#cardMiddle").innerHTML = `<span class="${colorClass}">${randomNumber}</span>`;
+        document.querySelector("#cardBottom").innerHTML = `<span class="${colorClass}">${randomSymbol}</span>`;
+        
+        cardGenerated += `${randomNumber}${randomSymbol}`;
+    }
+
+    
+    setInterval(generateRandomCard, 2000);
 };
+
+
+
+
+// window.onload = function() {
+//   //write your code here
+//   let symbols = ["♠", "♥", "♦", "♣"];
+//   let numbers = ["A","2","3","4","5","6","7","8","9","10","J","K","Q"];
+
+// let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+// let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+// function generateCard() {
+//   let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
+//   let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+//   document.querySelector(".top").innerHTML = `${randomSymbol}`;
+//   document.querySelector(".middle").textContent = `${randomNumber}`
+//   document.querySelector(".bottom").innerHTML = `${randomSymbol}`;
+// }
+
+// }
+
+
+
+
